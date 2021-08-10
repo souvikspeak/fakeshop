4c607ac081b9d40f251127892afcff6827fea84e
souvikDebnath <41965631+souvikspeak@users.noreply.github.com>
2021-08-10 22:30:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 not found!</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
